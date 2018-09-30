32a5ed42c9ab88829904d8e09e327a9834d4a6b4
Jason Liu <jason.m.liu.10@gmail.com>
2018-09-29 18:08:02 -0700

    test commit

---
+++ bot.js
@@ -0,0 +1,51 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        var parm = args[1]
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            case 'test':
+                //var request = new XMLHttpRequest();
+                //request.open('GET', 'GET https://api.pubgtracker.com/v2/search/steam?steamId={STEAM ID}', true);
+                //request.onload = function () {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: parm
+                    });
+                //}
+            break;
+
+            // Just add any case commands if you want to..
+         }
+     }
+});
